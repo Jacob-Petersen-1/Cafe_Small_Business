a1bce36c151b998ad3b0bf509b5e14c6c653a8a1
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-04-06 15:06:41 -0600

    Made migrations to the database + Comment out cafe.urls from urls.py for now to implement some further debugging

--- backend/drf_jwt_backend/cafe/models.py
+++ backend/drf_jwt_backend/cafe/models.py
@@ -5,7 +5,7 @@ class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    phone = models.CharField(11)
+    phone = models.CharField(max_length=11)
     email = models.CharField(max_length=20)
     message = models.CharField(max_length=280)
 
--- backend/drf_jwt_backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/drf_jwt_backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
-    path('', include('cafe.urls'))
+    # path('', include('cafe.urls'))
 ]
     # NOTES:
     # path('api/cars/', include('cars.urls')),
