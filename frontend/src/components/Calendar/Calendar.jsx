82ac1d352635bebecfbe3cee212bcdea42bd14cc
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-04-07 13:51:52 -0600

    Add some base components to project frontend Calendar/GoogleMap/WeatherWidget/

---
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import "./Calendar.css"
+
+const Calendar = () => {
+    return ( 
+        <div>Calendar</div>
+     );
+}
+ 
+export default Calendar ;
---
+++ frontend/src/components/GoogleMap/GoogleMap.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import "./GoogleMap.css"
+
+const GoogleMap = () => {
+    return ( 
+        <div>Google Map</div>
+     );
+}
+ 
+export default GoogleMap ;
