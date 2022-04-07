f29dc97c36c39222159b6b0a530136f9214a2467
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-04-07 12:36:56 -0600

    Work on Sending message to data base + Problem exists with JSON parse error + Will come back to research more options.

--- backend/drf_jwt_backend/cafe/urls.py
+++ backend/drf_jwt_backend/cafe/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('messages/',views.CustomerMessages.as_view()),
-    path('send/', views.SendMessage.as_view()),
+    path('messages/send/', views.SendMessage.as_view()),
     path('business/events/',views.BusinessEvents.as_view()),
     path('business/events/update/',views.BusinessEventUpdate.as_view()),
     path('personal/events/',views.PersonalEvents.as_view()),
