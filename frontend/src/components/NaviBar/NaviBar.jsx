9becc790664d1b929c0364f592eb4065dea3a296
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-04-10 18:17:26 -0600

    Adjust styling on Navbar component + Added links to components

--- frontend/src/components/NaviBar/NaviBar.jsx
+++ frontend/src/components/NaviBar/NaviBar.jsx
@@ -2,11 +2,11 @@ import "./NaviBar.css"
 
 const NaviBar = () => {
     return ( 
-        <div className="nav-container">
+        <div className="navbar">
             <div className="nav-left">
                 <ul className="link-left">
-                    <li>Home</li>
-                    <li>About</li>
+                    <li><a href="home">Home</a></li>
+                    <li><a href="about">About</a></li>
                 </ul>
             </div>
             <div className="nav-center">
@@ -14,8 +14,8 @@ const NaviBar = () => {
             </div>
             <div className="nav-right">
                 <ul className="link-right">
-                    <li>Contact</li>
-                    <li>Events</li>
+                    <li><a href="contact">Contact</a></li>
+                    <li><a href="events">Events</a></li>
                 </ul>
             </div>
         </div>
