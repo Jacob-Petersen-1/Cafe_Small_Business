da94553d2f2a181a5bc4fcb8ee49b5cdc5801a10
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-04-11 10:36:40 -0600

    Debugged issue where the calendar was not retrieving event data from data base + Adjust logic and implemented new solution old logic is added to notes at bottom of jsx component

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -8,40 +8,36 @@ import "./Calendar.css"
 
 const Calendar = () => {
  const [events, setEvents] = useState([]);
- const[titles, setTitles] = useState("");//Unused
- const[dates, setDates] = useState(""); //Unused
+
  
  
 
 
     useEffect(() => {
-        const fetchEvents = async () => {
-            try {
+        
+      const fetchEvents = async () => {
+          try {
               let response = await axios.get("http://127.0.0.1:8000/business/events/"); //Data from Database unprotected
-              let formatData = events.map( (e) => {
-                return {id:e.id.toString(), title:e.title, start: e.start, end: e.end}
-               })
-               console.log(formatData)
-              setEvents(formatData);
+              setEvents(response.data);
             } catch (error) {
               console.log(error.message);
             }
           };
-      fetchEvents();
+        fetchEvents();
         console.log("EVENT DATA:",events)
-    }, []);
-    
-  
-    
-    return ( 
+      }, []);
+      
+      
+      
+      return ( 
         <div className="calendar">
             <div className='calendar-main'>
                 <FullCalendar
                 plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
                 headerToolbar={{
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                  left: 'prev,next today',
+                  center: 'title',
+                  right: 'dayGridMonth,timeGridWeek,timeGridDay'
                 }}
                 initialView='dayGridMonth'
                 editable={true}
@@ -53,7 +49,12 @@ const Calendar = () => {
             </div>
         </div>
 
-     );
+);
 }
- 
+
 export default Calendar;
+// notes:
+// let formatData = events.map( (e) => {
+//   return {id:e.id.toString(), title:e.title, start: e.start, end: e.end}
+//  })
+//  console.log(formatData)
