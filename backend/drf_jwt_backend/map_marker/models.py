59a9049e5fc6176f9b2075e5432bb4be8e4d6480
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-04-12 10:50:11 -0600

    Adjust decimal field in models.py to accurately adjust marker lng and lat field

--- backend/drf_jwt_backend/map_marker/models.py
+++ backend/drf_jwt_backend/map_marker/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Marker(models.Model):
     address = models.CharField(max_length=50, null=True)
-    lat = models.DecimalField(max_digits=15,decimal_places=10)
-    lng = models.DecimalField(max_digits=15, decimal_places=10)
+    lat = models.DecimalField(max_digits=10,decimal_places=7)
+    lng = models.DecimalField(max_digits=10, decimal_places=7)
 
