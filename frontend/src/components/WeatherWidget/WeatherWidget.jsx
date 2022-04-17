14d923faab22994a082320fd62698c339438d9c1
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-04-17 02:30:52 -0600

    Add error handling to component WeatherWidget + Test seems to work

--- frontend/src/components/WeatherWidget/WeatherWidget.jsx
+++ frontend/src/components/WeatherWidget/WeatherWidget.jsx
@@ -23,7 +23,7 @@ const WeatherWidget = () => {
       console.log(error.message);
     };
   };
-   fetchData();
+  fetchData();
    console.log("weather: ", weather);
  }, []);
 
@@ -38,28 +38,29 @@ const WeatherWidget = () => {
 
    
  return (
-    <div>
-      <div class="card mb-3">
-        <h3 class="card-header">Weather</h3>
-           Update the location of the marker on the map to get weather for that location 
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">City: {weather.name}</li>
-          <li class="list-group-item">Temperature: {weather.main.temp} &deg;F</li>
-          <li class="list-group-item">Feels Like: {weather.main.feels_like} &deg;F</li>
-          <li class="list-group-item">Temperature Highs: {weather.main.temp_max} &deg;F</li>
-          <li class="list-group-item">Temperature Lows: {weather.main.temp_min} &deg;F</li>
-          <li class="list-group-item">Wind: {weather.wind.speed} MPH</li>
-          <li class="list-group-item">Wind Gust: {weather.wind.gust} MPH</li>
-          <li class="list-group-item">Humidity: {weather.main.humidity}%</li>
-          <li class="list-group-item">Description: {weather.weather[0].main}</li>
-        </ul>
-        <div class="card-footer text-muted">Today</div>
-      </div>
-    </div>
+   <div>
+     {(typeof weather.main != 'undefined') ? (
+     <div class="card mb-3">
+       <h3 class="card-header">Weather</h3>
+          Update the location of the marker on the map to get weather for that location 
+       <ul class="list-group list-group-flush">
+         <li class="list-group-item">City: {weather.name}</li>
+         <li class="list-group-item">Temperature: {weather.main.temp} &deg;F</li>
+         <li class="list-group-item">Feels Like: {weather.main.feels_like} &deg;F</li>
+         <li class="list-group-item">Temperature Highs: {weather.main.temp_max} &deg;F</li>
+         <li class="list-group-item">Temperature Lows: {weather.main.temp_min} &deg;F</li>
+         <li class="list-group-item">Wind: {weather.wind.speed} MPH</li>
+         <li class="list-group-item">Wind Gust: {weather.wind.gust} MPH</li>
+         <li class="list-group-item">Humidity: {weather.main.humidity}%</li>
+         <li class="list-group-item">Description: {weather.weather[0].main}</li>
+       </ul>
+       <div class="card-footer text-muted">Today</div>
+     </div>
+     ):(
+       <div>Weather</div>
+     )}
+  </div>
 
-   
-    
- 
 
 
   );
